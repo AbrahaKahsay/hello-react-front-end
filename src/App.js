d27d8dac7d4fb52dbbd767592ebac2f08d0576a8
AbrahaKahsay <abrahak81@gmail.com>
2022-12-03 13:29:15 +0300

    Create and modify App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/greeting';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/greeting" element={<Greeting />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
